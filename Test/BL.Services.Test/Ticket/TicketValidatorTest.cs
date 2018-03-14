fc7b077740c8e1db9c7c638be16abe3825b75dd3
JerryChuang <jerrychuang@nine-yi.com>
2018-03-14 11:05:06 +0800

    CompleteVersion - 撰寫Test_CheckFileData測試案例，驗證檢查條碼檔案資料，其中一筆資料已存在於同份檔案

--- Test/BL.Services.Test/Ticket/TicketValidatorTest.cs
+++ Test/BL.Services.Test/Ticket/TicketValidatorTest.cs
@@ -181,6 +181,58 @@ namespace FirstUnitTest.BL.Services.Test.Ticket
         }
 
         /// <summary>
+        /// 驗證檢查條碼檔案資料，其中一筆資料已存在於同份檔案
+        /// </summary>
+        [Fact]
+        public void Test_CheckFileData_HasOneData_ExistInSameFile_Invalidation()
+        {
+            //// Arrange
+            var stubTicketEntity = new TicketEntity
+            {
+                BarCodeTypeDef = "CODE_39",
+                CodeNumber = 3
+            };
+
+            var sourceDataTable = this.GeneratorThreeBarCode39BaseDataTable();
+
+            sourceDataTable.Rows[0]["TicketSlave_Code1"] = "AB2018";
+            sourceDataTable.Rows[0]["TicketSlave_Code2"] = "0102";
+            sourceDataTable.Rows[0]["TicketSlave_Code3"] = "/+00001";
+            sourceDataTable.Rows[1]["TicketSlave_Code1"] = "AB2018";
+            sourceDataTable.Rows[1]["TicketSlave_Code2"] = "0102";
+            sourceDataTable.Rows[1]["TicketSlave_Code3"] = "/+00001";
+
+            var expectedDataTable = this.GeneratorThreeBarCode39BaseDataTable();
+            expectedDataTable.Rows[0]["TicketSlave_Code1"] = "AB2018";
+            expectedDataTable.Rows[0]["TicketSlave_Code2"] = "0102";
+            expectedDataTable.Rows[0]["TicketSlave_Code3"] = "/+00001";
+            expectedDataTable.Rows[1]["TicketSlave_Code1"] = "AB2018";
+            expectedDataTable.Rows[1]["TicketSlave_Code2"] = "0102";
+            expectedDataTable.Rows[1]["TicketSlave_Code3"] = "/+00001";
+
+            expectedDataTable.Rows[1]["TicketSlave_StatusTypeDef"] =
+                TicketSlaveStatusEnum.ExistInSameFile;
+
+            expectedDataTable.Rows[1]["TicketSlave_InvalidationData"] =
+                string.Format(
+                    "{0},{1},{2}",
+                    expectedDataTable.Rows[1]["TicketSlave_Code1"],
+                    expectedDataTable.Rows[1]["TicketSlave_Code2"],
+                    expectedDataTable.Rows[1]["TicketSlave_Code3"]);
+
+            var target = new TicketValidator();
+
+            //// Act
+            target.CheckFileData(stubTicketEntity, sourceDataTable);
+
+            //// Assert
+            var expectedItemArray = expectedDataTable.AsEnumerable().Select(x => x.ItemArray);
+            var actualItemArray = sourceDataTable.AsEnumerable().Select(x => x.ItemArray);
+
+            actualItemArray.ShouldBeEquivalentTo(expectedItemArray);
+        }
+
+        /// <summary>
         /// 產生條碼檔案資料，三組條碼格式為CODE_128的基本資料
         /// </summary>
         /// <returns>條碼檔案資料，三組條碼格式為CODE_128的基本資料</returns>
@@ -213,6 +265,58 @@ namespace FirstUnitTest.BL.Services.Test.Ticket
                 "XY00003",
                 "XY00004",
                 "XY00005"
+            };
+
+            for (int i = 0; i < code1List.Length; i++)
+            {
+                var row = dataTable.NewRow();
+
+                row[0] = 1L;
+                row[1] = code1List[i];
+                row[2] = code2List[i];
+                row[3] = code3List[i];
+                row[4] = "Normal";
+                row[5] = string.Empty;
+
+                dataTable.Rows.Add(row);
+            }
+
+            return dataTable;
+        }
+
+        /// <summary>
+        /// 產生條碼檔案資料，三組條碼格式為CODE_39的基本資料
+        /// </summary>
+        /// <returns>條碼檔案資料，兩組條碼格式為CODE_39的基本資料</returns>
+        private DataTable GeneratorThreeBarCode39BaseDataTable()
+        {
+            var dataTable = this.GeneratorBarCodBaseDataTable();
+
+            string[] code1List = new string[]
+            {
+                "AB2018",
+                "AB2018",
+                "AB2018",
+                "AB2018",
+                "AB2018"
+            };
+
+            string[] code2List = new string[]
+            {
+                "0102",
+                "0102",
+                "0102",
+                "0102",
+                "0102"
+            };
+
+            string[] code3List = new string[]
+            {
+                "/+00001",
+                "/+00002",
+                "/+00003",
+                "/+00004",
+                "/+00005"
             };
 
             for (int i = 0; i < code1List.Length; i++)
