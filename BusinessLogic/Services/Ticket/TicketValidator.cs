05eeec0e5652bd43f9d04a54bcf819345de43946
JerryChuang <jerrychuang@nine-yi.com>
2018-03-14 10:38:55 +0800

    CompleteVersion - 實作CheckFileData方法，通過測試「資料長度超過上限」案例

--- BusinessLogic/Services/Ticket/TicketValidator.cs
+++ BusinessLogic/Services/Ticket/TicketValidator.cs
@@ -31,6 +31,49 @@ namespace FirstUnitTest.BL.Services.Ticket
         /// <param name="ticketSlaveCodeData">票券主檔資料暫存資料清單</param>
         public void CheckFileData(TicketEntity ticketEntity, DataTable ticketSlaveCodeData)
         {
+            var barCodeNumber = ticketEntity.CodeNumber;
+
+            BarcodeFormat barCodeType = this.GetBarcodeFormatEnum(ticketEntity);
+
+            var verificationCodeHashSet = new HashSet<string>();
+
+            foreach (DataRow row in ticketSlaveCodeData.Rows)
+            {
+                string[] codeList = new string[]
+                {
+                    (string) row["TicketSlave_Code1"],
+                    (string) row["TicketSlave_Code2"],
+                    (string) row["TicketSlave_Code3"]
+                };
+
+                var rowStatus = (string)row["TicketSlave_StatusTypeDef"];
+                TicketSlaveStatusEnum status;
+                if (Enum.TryParse(rowStatus, out status) == false)
+                {
+                    var errorMessage = $"條碼來源子檔狀態類型不正確，Ticket_Id: {ticketEntity.Id}、TicketSlave_StatusTypeDef: {rowStatus}";
+                    throw new ArgumentException(errorMessage);
+                }
+
+                //// 依據條碼組數設定，決定要檢查到第幾組條碼
+                for (int index = 0; index < barCodeNumber; index++)
+                {
+                    if (status == TicketSlaveStatusEnum.Normal)
+                    {
+                        status = this.CheckTicketCode(codeList[index], barCodeType);
+                        if (status != TicketSlaveStatusEnum.Normal)
+                        {
+                            row["TicketSlave_StatusTypeDef"] = status.ToString();
+                        }
+                    }
+                }
+
+                if (status != TicketSlaveStatusEnum.Normal)
+                {
+                    var invalidationData = string.Join(",", codeList).TrimEnd(',');
+
+                    row["TicketSlave_InvalidationData"] = invalidationData;
+                }
+            }
         }
 
         /// <summary>
