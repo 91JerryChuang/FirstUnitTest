26264b0a8bf8d61dbb03f80ab66d43510ca0fea9
JerryChuang <jerrychuang@nine-yi.com>
2018-03-14 10:40:34 +0800

    CompleteVersion - 撰寫Test_CheckFileData測試案例，驗證檢查條碼檔案資料，三組條碼格式為CODE_128，其中一筆資料不符合CODE_128的格式

--- Test/BL.Services.Test/Ticket/TicketValidatorTest.cs
+++ Test/BL.Services.Test/Ticket/TicketValidatorTest.cs
@@ -54,6 +54,48 @@ namespace FirstUnitTest.BL.Services.Test.Ticket
         }
 
         /// <summary>
+        /// 驗證檢查條碼檔案資料，三組條碼格式為CODE_128，其中一筆資料不符合CODE_128的格式
+        /// </summary>
+        [Fact]
+        public void Test_CheckFileData_ThreeCode128_HasOneData_InvalidationCodeFormat_Invalidation()
+        {
+            //// Arrange
+            var stubTicketEntity = new TicketEntity
+            {
+                BarCodeTypeDef = "CODE_128",
+                CodeNumber = 3
+            };
+
+            var sourceDataTable = this.GeneratorThreeBarCode128BaseDataTable();
+
+            sourceDataTable.Rows[1]["TicketSlave_Code2"] = "九易App0001";
+
+            var expectedDataTable = this.GeneratorThreeBarCode128BaseDataTable();
+            expectedDataTable.Rows[1]["TicketSlave_Code2"] = "九易App0001";
+
+            expectedDataTable.Rows[1]["TicketSlave_StatusTypeDef"] =
+                TicketSlaveStatusEnum.InvalidationCodeFormat;
+
+            expectedDataTable.Rows[1]["TicketSlave_InvalidationData"] =
+                string.Format(
+                    "{0},{1},{2}",
+                    expectedDataTable.Rows[1]["TicketSlave_Code1"],
+                    expectedDataTable.Rows[1]["TicketSlave_Code2"],
+                    expectedDataTable.Rows[1]["TicketSlave_Code3"]);
+
+            var target = new TicketValidator();
+
+            //// Act
+            target.CheckFileData(stubTicketEntity, sourceDataTable);
+
+            //// Assert
+            var expectedItemArray = expectedDataTable.AsEnumerable().Select(x => x.ItemArray);
+            var actualItemArray = sourceDataTable.AsEnumerable().Select(x => x.ItemArray);
+
+            actualItemArray.ShouldBeEquivalentTo(expectedItemArray);
+        }
+
+        /// <summary>
         /// 產生條碼檔案資料，三組條碼格式為CODE_128的基本資料
         /// </summary>
         /// <returns>條碼檔案資料，三組條碼格式為CODE_128的基本資料</returns>
